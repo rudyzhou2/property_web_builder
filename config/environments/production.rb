41f47018b83eed1601fbf441ede3bea8c5aa5209
Rudy Zhou <qian.q.zhou@mail.mcgill.ca>
2023-12-06 11:10:07 -0500

    Update production.rb add action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "sandboxc85f4c6cf2ce42e9ae0225ee3a1b7de3.mailgun.org",
+    :authentication => :plain,
+    :user_name => Rails.application.secrets.sendgrid_username,
+    :password => Rails.application.secrets.sendgrid_password
+    # If deploying to heroku, set below in secrets.yml so above works
+    sendgrid_username: <%= ENV['SENDGRID_USERNAME'] %>
+    sendgrid_password: <%= ENV['SENDGRID_PASSWORD'] %>
+  }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
